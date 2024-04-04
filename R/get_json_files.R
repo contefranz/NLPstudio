d1d86b59782b36f4387c235776ad178e51db12e5
contefranz <francesco.grossetti@unibocconi.it>
2024-04-04 17:53:23 +0200

    get_json_files can take more general patterns. Argument pattern defaults to NULL now.

--- R/get_json_files.R
+++ R/get_json_files.R
@@ -4,7 +4,7 @@
 #'
 #' @param root_path Character giving the path to the root folder location (see 'Details').
 #' @param pattern An optional \link[base]{regex}. Only file names which match the regular expression will be returned
-#' as in \code{\link[base]{list.files}}.
+#' as in \code{\link[base]{list.files}}. Default to \code{NULL}.
 #' @param fyear Optional numeric vector specifying the time window in fiscal years. When \code{NULL},
 #' all the fiscal years detected in \code{root_path} are considered. Default to \code{NULL}.
 #'
@@ -18,15 +18,22 @@
 #' @return A list whose elements are the fiscal years. Each element contains the pointers to each JSON file within
 #' each fiscal year.
 #'
-#' @importFrom stringr str_c str_locate str_replace
+#' @importFrom stringr str_remove str_c str_locate str_extract
 #'
 #' @export
 
-get_json_files = function(root_path, pattern, fyear = NULL) {
+get_json_files = function(root_path, pattern = NULL, fyear = NULL) {
 
+  # remove the trailing "/" at the end of the path to avoid double slashes in the call of list.files
+  root_path = str_remove(root_path, "/$")
+
+  # if fyear specifies a followup, modifies the search pattern
   if (!is.null(fyear)) {
-    # if fyear specifies a followup, modifies the search pattern
-    pattern = str_c(pattern, ".*", fyear, collapse = "|")
+    if (!is.null(pattern)) {
+      pattern = str_c(pattern, ".*", fyear, collapse = "|")
+    } else {
+      pattern = str_c(fyear, collapse = "|")
+    }
   }
   thedirs = list.files(root_path, pattern = pattern,
                        recursive = FALSE,
@@ -36,11 +43,13 @@ get_json_files = function(root_path, pattern, fyear = NULL) {
   thedirs_list = vector("list", ndirs)
   # attach a name given by the basename of each directory path
   names(thedirs_list) = basename(thedirs)
+  # build the followup time window
+  followup = str_extract(names(thedirs_list), "\\d+")
 
   # for each of the detected directory, extract the pointers to each JSON file.
   # then, return the container as a named list.
   for ( i_year in seq_along(thedirs_list) ) {
-    message("Processing year ", fyear[i_year])
+    message("Processing ", followup[i_year])
     current_path = thedirs[i_year]
     current_files = list.files(current_path,
                                recursive = FALSE,
