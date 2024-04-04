b98ece793914953e09fea171f9c2a9cf8a524723
contefranz <francesco.grossetti@unibocconi.it>
2024-04-04 14:35:33 +0200

    Add JSON container function

--- R/get_json_files.R
+++ R/get_json_files.R
@@ -1,9 +1,19 @@
-#' List JSON files within the tree structure
+#' JSON Files Container
 #'
-#' Convenient function to list all the JSON files contained in a given folder.
+#' Create a container list with the pointers to all the JSON files detected in the input folder.
 #'
-#' @param path_in Character giving the path to folder location.
-#' @param fyear Optional numeric vector specifying the time window in fiscal years. Default to \code{NULL}.
+#' @param root_path Character giving the path to the root folder location (see 'Details').
+#' @param pattern An optional \link[base]{regex}. Only file names which match the regular expression will be returned
+#' as in \code{\link[base]{list.files}}.
+#' @param fyear Optional numeric vector specifying the time window in fiscal years. When \code{NULL},
+#' all the fiscal years detected in \code{root_path} are considered. Default to \code{NULL}.
+#'
+#' @details
+#' The function works in conjunction with the EDGAR crawler structure. In general, this creates three
+#' main folders as containers: (i) the original HTML files, (ii) the metadata of each filing, (iii)
+#' the extracted items in JSON format. Each of these structures is usually, and conveniently, organized
+#' by fiscal year. This is not an imposition but rather a strong suggestion. Ideally, one could put
+#' all the filings into the root_path
 #'
 #' @return A list whose elements are the fiscal years. Each element contains the pointers to each JSON file within
 #' each fiscal year.
@@ -12,27 +22,26 @@
 #'
 #' @export
 
-get_json_files = function(path_in, fyear = NULL) {
+get_json_files = function(root_path, pattern, fyear = NULL) {
 
-  if (is.null(fyear)) {
-    thedirs = list.dirs(path_in, recursive = FALSE)
-    fyear = as.integer(thedirs)
-  } else {
-    fyear_regex = str_c(fyear, collapse = "|")
-    thedirs = list.files(path_in, recursive = FALSE, include.dirs = TRUE, pattern = fyear_regex)
+  if (!is.null(fyear)) {
+    # if fyear specifies a followup, modifies the search pattern
+    pattern = str_c(pattern, ".*", fyear, collapse = "|")
   }
+  thedirs = list.files(root_path, pattern = pattern,
+                       recursive = FALSE,
+                       include.dirs = TRUE,
+                       full.names = TRUE)
   ndirs = length(thedirs)
   thedirs_list = vector("list", ndirs)
-  names(thedirs_list) = thedirs
+  # attach a name given by the basename of each directory path
+  names(thedirs_list) = basename(thedirs)
 
+  # for each of the detected directory, extract the pointers to each JSON file.
+  # then, return the container as a named list.
   for ( i_year in seq_along(thedirs_list) ) {
     message("Processing year ", fyear[i_year])
-    current_path = file.path(path_in, fyear[i_year])
-    current_path2 = file.path(path_in, fyear[i_year])
-    find_double_slashes = str_locate(current_path, "//")
-    if ( any(!is.na(find_double_slashes)) ) {
-      current_path = str_replace(current_path, pattern = "//", replacement = "/")
-    }
+    current_path = thedirs[i_year]
     current_files = list.files(current_path,
                                recursive = FALSE,
                                full.names = TRUE)
