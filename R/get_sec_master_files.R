04ed9d2edd102854f03404d564de00fb7c06f205
contefranz <francesco.grossetti@unibocconi.it>
2024-04-05 10:00:52 +0200

    New function to collect metadata as SEC master files

---
+++ R/get_sec_master_files.R
@@ -0,0 +1,101 @@
+#' SEC Master Files Container
+#'
+#' Create a container list with the pointers to all the JSON files detected in the input folder.
+#'
+#' @param root_path Character giving the path to the root folder location (see 'Details').
+#' @param pattern An optional \link[base]{regex}. Only file names which match the regular expression will be returned
+#' as in \code{\link[base]{list.files}}. Default to \code{NULL}.
+#' @param fyear Optional numeric vector specifying the time window in fiscal years. When \code{NULL},
+#' all the fiscal years detected in \code{root_path} are considered. Default to \code{NULL}.
+#'
+#' @details
+#' The function works in conjunction with the EDGAR crawler structure. In general, this creates three
+#' main folders as containers: (i) the original HTML files, (ii) the metadata of each filing, (iii)
+#' the extracted items in JSON format. Each of these structures is usually, and conveniently, organized
+#' by fiscal year. This is not an imposition but rather a strong suggestion. Ideally, one could put
+#' all the filings into the root_path and work from that.
+#'
+#' This function works on the metadata files, also called SEC master files. Typically, such data are
+#' stored in a .csv file and are located in \code{root_path} with no sub-directory. It is recommended
+#' to specify at least a \code{pattern} to detect those files.
+#'
+#' @return A \code{data.table} containing all the SEC master files as detected by the function
+#' with the following columns:
+#'
+#' \item{\code{cik}}{The Central Index Key as given by the SEC EDGAR database. Integer.}
+#' \item{\code{cname}}{The company name. Character.}
+#' \item{\code{type}}{The filing type. Character.}
+#' \item{\code{date_filed}}{The filing date on the SEC. IDat.}
+#' \item{\code{fyear_end}}{The fiscal year end date. IDat.}
+#' \item{\code{sic}}{The four-digit SIC industry code. Integer.}
+#' \item{\code{state_of_inc}}{U.S. state of incorporation (abbreviation). Character.}
+#' \item{\code{state_location}}{U.S. state location (abbreviation). Character.}
+#' \item{\code{filing_detail}}{Pointer to filing summary page in HTML. Character.}
+#' \item{\code{filing_html}}{Pointer to filing in HTML. Character.}
+#' \item{\code{filing_txt}}{Pointer to complete submission file in txt. Character.}
+#' \item{\code{filename}}{Filename. Character.}
+#'
+#' @author Francesco Grossetti \email{francesco.grossetti@@unibocconi.it}
+#'
+#' @import data.table
+#' @importFrom stringr str_remove str_c str_replace_all str_to_lower
+#'
+#' @export
+
+
+get_sec_master_files = function(root_path, pattern = NULL, fyear = NULL) {
+
+  # remove the trailing "/" at the end of the path to avoid double slashes in the call of list.files
+  root_path = str_remove(root_path, "/$")
+
+  # if fyear specifies a followup, modifies the search pattern
+  if (!is.null(fyear)) {
+    if (!is.null(pattern)) {
+      pattern = str_c(pattern, ".*", fyear, collapse = "|")
+    } else {
+      pattern = str_c(fyear, collapse = "|")
+    }
+  }
+  thefiles = list.files(root_path, pattern = pattern,
+                        recursive = FALSE,
+                        full.names = TRUE)
+  n_files = length(thefiles)
+  master_collector = vector("list", n_files)
+
+  for ( i_file in seq_len(n_files) ) {
+    current_pointer = basename(thefiles[i_file])
+    message("Reading file ", current_pointer)
+    current = fread(thefiles[i_file])
+    master_collector[[ i_file ]] = current
+  }
+
+  master_all = rbindlist(master_collector)
+
+  # check that only one filing type exists. If the check fails, stop with an error as the selection
+  # of the filing type is inherited from the edgar-crawler and it is not controlled here in R nor
+  # by edgartools
+  if( uniqueN(master_collector$Type) > 1 ) {
+    stop("Multiple filing types detected")
+  }
+
+  # fixing column names to match those from the JSON structures as modified by from_json_to_df()
+  # 1. replace space with "_"
+  setnames(master_all, new = str_replace_all(names(master_all), "\\s", "_"))
+  # 2. lowercase everything
+  setnames(master_all, new = str_to_lower(names(master_all)))
+  # 3. renaming
+  setnames(master_all,
+           c("company", "complete_text_file_link", "html_index", "filing_date", "htm_file_link", "period_of_report"),
+           c("cname", "filing_txt", "filing_detail", "date_filed", "filing_html", "fyear_end") )
+  # 4. remove useless columns
+  master_all[ , `:=` (date = NULL, fiscal_year_end = NULL)]
+  # 5. define a column order that resembles that of the JSON structure
+  setcolorder(master_all, c("cik", "cname", "type", "date_filed", "fyear_end", "sic", "state_of_inc",
+                            "state_location", "filing_detail", "filing_html", "filing_txt", "filename"))
+
+  # note that date columns are in the efficient and fast format IDat which is proper of data.table
+  # Make sure that the output of from_json_to_df() has that too!
+
+  return(master_all[])
+
+}
