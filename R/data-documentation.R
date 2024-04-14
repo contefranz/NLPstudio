b0ce15b8e3fd57c514868f7c1613e1097600c97f
Francesco Grossetti <francesco.grossetti@unibocconi.it>
2024-04-14 22:20:05 +0200

    Add forward-looking statement data; LazyData is now True in DESCRIPTION

--- R/data-documentation.R
+++ R/data-documentation.R
@@ -8,3 +8,29 @@
 #'   \emph{Journal of Financial and Quantitative Analysis}, Forthcoming
 #' @keywords data
 "data_dictionary_LoughranMcDonald_Complexity"
+
+#' Feng Li Forward Looking Statement Dictionary
+#'
+#' A \code{\link[quanteda]{dictionary}} containing the 
+#' the 2010 version of the Feng Li's Forward Looking Statement Word List. The list contains 18 words.
+#' @source <https://onlinelibrary.wiley.com/doi/abs/10.1111/j.1475-679X.2010.00382.x>
+#' @references
+#'   Li, F. (2010). The Information Content of Forward-Looking Statements in 
+#'   Corporate Filings—A Naïve Bayesian Machine Learning Approach,
+#'   \emph{Journal of Accounting Research}, 48 (5), 1049--1102
+#' @keywords data
+"data_dictionary_Li_FLS"
+
+#' Bozanic Roulstone VanBuskirk Forward Looking Statement Dictionary
+#'
+#' A \code{\link[quanteda]{dictionary}} containing the 
+#' the 2018 version of the Bozanic, Roulstone, and VanBuskirk Forward Looking Statement Word List. 
+#' The list contains 90 words.
+#' @source <https://www.sciencedirect.com/science/article/abs/pii/S0165410117300733>
+#' @references
+#'   Bozanic, Z., Roulstone, D.T., Van Buskirk, A. (2018), Management earnings forecasts and other 
+#'   forward-looking statements, \emph{Journal of Accounting and Economics}, Volume 65, Issue 1, 2018,
+#'   Pages 1-20,
+#' @keywords data
+"data_dictionary_BozanicRoulstoneVanBuskirk_FLS"
+
