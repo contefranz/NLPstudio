3620f145f2ddad3949b5c4e32f8237356d6d4096
Francesco Grossetti <francesco.grossetti@unibocconi.it>
2024-04-18 22:14:34 +0200

    Add links in documentation

--- R/data-documentation.R
+++ R/data-documentation.R
@@ -2,10 +2,14 @@
 #'
 #' A \code{\link[quanteda]{dictionary}} containing the
 #' the 2024 version of the Loughran and McDonald Firm Complexity Word List. The list contains 53 words.
-#' @source <https://sraf.nd.edu/complexity/>
+#' @source 
+#' <https://sraf.nd.edu/complexity/>
+#' 
+#' <https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3645372>
 #' @references
 #'   Loughran, T. & McDonald, B. (2024). Measuring Firm Complexity,
-#'   \emph{Journal of Financial and Quantitative Analysis}, Forthcoming.
+#'   \emph{Journal of Financial and Quantitative Analysis}, 
+#'   [Forthcoming](https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3645372).
 #' @keywords data
 "data_dictionary_LoughranMcDonald_Complexity"
 
