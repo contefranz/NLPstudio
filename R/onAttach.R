fbd313ebd945e160ba6e62e0f0c0e5f3c38359d8
Francesco Grossetti <francesco.grossetti@unibocconi.it>
2025-02-17 17:32:39 +0100

    Some polishing of old files and information related to edgartools.

--- R/onAttach.R
+++ R/onAttach.R
@@ -2,8 +2,8 @@ core <- c("quanteda", "quanteda.textstats", "spacyr", "data.table", "stringr", "
 
 
 .onAttach <- function(...) {
-  attached <- edgartools_attach()
-  message(edgartools_attach_message(attached))
+  attached <- NLPstudio_attach()
+  message(NLPstudio_attach_message(attached))
 }
 
 is_attached <- function(x) {
@@ -11,7 +11,7 @@ is_attached <- function(x) {
 }
 
 is_loading_for_tests <- function() {
-  !interactive() && identical(Sys.getenv("DEVTOOLS_LOAD"), "edgartools")
+  !interactive() && identical(Sys.getenv("DEVTOOLS_LOAD"), "NLPstudio")
 }
 
 core_unloaded <- function() {
@@ -19,46 +19,46 @@ core_unloaded <- function() {
   core[!search %in% search()]
 }
 
-# Attach the package from the same package library it was loaded from before. 
+# Attach the package from the same package library it was loaded from before.
 same_library <- function(pkg) {
   loc <- if (pkg %in% loadedNamespaces()) dirname(getNamespaceInfo(pkg, "path"))
   library(pkg, lib.loc = loc, character.only = TRUE, warn.conflicts = FALSE)
 }
 
-edgartools_attach <- function() {
+NLPstudio_attach <- function() {
   to_load <- core_unloaded()
-  
+
   suppressPackageStartupMessages(
     lapply(to_load, same_library)
   )
-  
+
   invisible(to_load)
 }
 
-edgartools_attach_message <- function(to_load) {
+NLPstudio_attach_message <- function(to_load) {
   if (length(to_load) == 0) {
     return(NULL)
   }
-  
+
   header <- cli::rule(
-    left = cli::style_bold("Attaching core edgartools packages"),
-    right = paste0("edgartools ", package_version_h("edgartools"))
+    left = cli::style_bold("Attaching core NLPstudio packages"),
+    right = paste0("NLPstudio ", package_version_h("NLPstudio"))
   )
-  
+
   to_load <- sort(to_load)
   versions <- vapply(to_load, package_version_h, character(1))
-  
+
   packages <- paste0(
     cli::col_green(cli::symbol$tick), " ", cli::col_br_cyan(format(to_load)), " ",
     cli::ansi_align(versions, max(cli::ansi_nchar(versions)))
   )
-  
+
   if (length(packages) %% 2 == 1) {
     packages <- append(packages, "")
   }
   col1 <- seq_len(length(packages) / 2)
   info <- paste0(packages[col1], "     ", packages[-col1])
-  
+
   paste0(header, "\n", paste(info, collapse = "\n"))
 }
 
@@ -68,12 +68,12 @@ package_version_h <- function(pkg) {
 
 highlight_version <- function(x) {
   x <- as.character(x)
-  
+
   is_dev <- function(x) {
     x <- suppressWarnings(as.numeric(x))
     !is.na(x) & x >= 9000
   }
-  
+
   pieces <- strsplit(x, ".", fixed = TRUE)
   pieces <- lapply(pieces, function(x) ifelse(is_dev(x), cli::col_red(x), x))
   vapply(pieces, paste, collapse = ".", FUN.VALUE = character(1))
