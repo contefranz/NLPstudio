d2cba9c871ee7caf9995c7b877061d10100812ad
Francesco Grossetti <francesco.grossetti@unibocconi.it>
2025-09-27 08:49:45 +0200

    Fixed document ordering and argument logging in reshape_corpus. Also, defaulting to sentences

--- R/reshape_corpus.R
+++ R/reshape_corpus.R
@@ -2,23 +2,27 @@
 #'
 #' Reshape a [corpus] in parallel under the **[future]** paradigm.
 #'
-#' @param x A **quanteda** [corpus] as built by `create_corpus`.
+#' @param x A **quanteda** [corpus].
 #' @param ncores The number of [multisession] workers to be allocated for the reshaping.
-#' @param ... Additional arguments passed to [corpus_reshape] (see 'Details').
+#' @param to Character; type of reshaping. Default is `"sentences"`. Passed to [corpus_reshape()].
+#' @param ... Additional arguments passed to [corpus_reshape()] (see 'Details').
 #'
 #' @details
-#' This function is a wrapper of [corpus_reshape] but it is much faster as it
-#' uses the \href{https://www.futureverse.org/packages-overview.html}{\strong{future}}
-#' paradigm to parallelize the task.
+#' This function wraps [quanteda::corpus_reshape()] but executes in parallel using the
+#' [future] framework. The most common use is reshaping to sentences (`to = "sentences"`),
+#' which enables sentence-level text analysis and alignment with document-level metadata.
 #'
-#' Use `...` to control the reshaping process. For instance, whether to reshape the corpus to
-#' sentences (`to = "sentences"`) which is likely the most common usage.
+#' Document order is guaranteed to match the input `corpus`. After reshaping,
+#' documents are reordered to ensure consistency with the original corpus ordering.
+#'
+#' Use `...` to further control the reshaping process. For instance, one can respahe the corpus to
+#' paragraphs by passing `to = "paragraphs"`.
 #'
 #' @returns A reshaped [corpus] object.
 #'
 #' @author Francesco Grossetti \email{francesco.grossetti@@unibocconi.it}
 #'
-#' @importFrom quanteda is.corpus corpus_reshape
+#' @importFrom quanteda is.corpus corpus_reshape ndoc
 #' @importFrom future plan multisession sequential
 #' @importFrom stringr str_remove
 #' @importFrom future.apply future_lapply
@@ -26,7 +30,7 @@
 #' @export
 
 
-reshape_corpus = function(x, ncores, ...) {
+reshape_corpus = function(x, ncores, to = "sentences", ...) {
 
   if ( !is.corpus(x) ) {
     stop("x must be a quanteda corpus object")
@@ -39,8 +43,8 @@ reshape_corpus = function(x, ncores, ...) {
   } else {
     cli_alert_info("quanteda::corpus_reshape() has been called with the following parameters")
     args_active = paste0(names(args), " = ", unlist(args))
-    for (iarg in seq_along(args_active) ) {
-      cli_alert("{args_active[iarg]}")
+    for (nm in names(args)) {
+      cli_alert("{nm} = {toString(args[[nm]])}")
     }
   }
 
@@ -48,8 +52,16 @@ reshape_corpus = function(x, ncores, ...) {
   plan(multisession, workers = ncores)
   chunks = split(x, rep_len(1L:ncores, ndoc(x)))
 
-  reshaped = do.call(c, future_lapply(chunks, corpus_reshape, future.seed = TRUE, ...))
+  reshaped <- do.call(c, future_lapply(
+    chunks, corpus_reshape, to = to, future.seed = TRUE, ...
+  ))
   plan(sequential)
+  
+  # enforce original document ordering
+  orig_docs <- docnames(x)
+  reshaped <- reshaped[order(match(
+    str_remove(docnames(reshaped), "\\.\\d+$"), orig_docs
+  ))]
 
   doc_names = sort(docnames(x))
   doc_names_reshaped = sort(unique(str_remove(docnames(reshaped), "\\.\\d+")))
