9b554c8434b93a0df43d9d312c6c34686b360eaa
Francesco Grossetti <francesco.grossetti@unibocconi.it>
2025-09-30 17:30:02 +0200

    lookup_tokens now uses PSOCK as a dynamic parallel backend in place of future

--- R/lookup_tokens.R
+++ R/lookup_tokens.R
@@ -1,53 +1,82 @@
 #' Fast Tokens Lookup
 #'
-#' Lookup a [tokens] object in parallel via **[future]**.
+#' Lookup tokens in parallel using backends from the **parallel**
+#' package. By default, lookup is parallelized with PSOCK clusters
+#' ([parallel::clusterApplyLB()]) for stable cross-platform performance
+#' and dynamic load balancing. Optionally, FORK-based parallelism
+#' ([parallel::mclapply()]) may be requested on Linux/macOS, but this
+#' can lead to instability with quanteda (see *Note*).
+#' @inheritParams tokenize_corpus
+#' @param ... Additional arguments passed to [quanteda::tokens_lookup()].
 #'
-#' @param x A **quanteda** [tokens] object.
-#' @param ncores The number of [multisession] workers to be allocated for 
-#' the lookup.
-#' @param ... Additional arguments passed to [tokens_lookup].
+#' @details
+#' More details discussing the parallel strategy are given in [tokenize_corpus()].
 #'
-#' @returns A [tokens] object.
+#' @note
+#' By default, `socket = "PSOCK"`. Using `socket = "FORK"` on Linux/macOS
+#' may be faster but is discouraged when tokenizing large corpora with
+#' quanteda, as it can lead to undefined behavior. If you insist on using
+#' `socket = "FORK"`, consider setting environment variables such as
+#' `OMP_NUM_THREADS=1` and/or `quanteda_options(threads = 1)`) to reduce conflicts. 
+#' On Windows, setting `socket = "FORK"` will result in an error.
 #'
-#' @author Francesco Grossetti \email{francesco.grossetti@@unibocconi.it}
+#' @return A [quanteda::tokens()] object with lookups applied and
+#' documents in the same order as the input.
 #'
-#' @importFrom quanteda is.dictionary is.tokens tokens_lookup docnames
-#' @importFrom future plan multisession sequential
-#' @importFrom future.apply future_lapply
-#' @importFrom cli cli_h2 cli_h3 cli_alert_info cli_alert cli_alert_success cli_alert_danger
+#' @importFrom quanteda is.dictionary is.tokens tokens_lookup ndoc docnames
+#' @importFrom parallel makeCluster stopCluster clusterApplyLB clusterExport mclapply
+#' @importFrom cli cli_h2 cli_alert_info cli_alert_success
 #' @export
-
-
-lookup_tokens = function(x, ncores, ...) {
-  
-  if ( !is.tokens(x) ) {
+lookup_tokens <- function(x, ncores = 1, nchunks = ncores, socket = c("PSOCK", "FORK"), ...) {
+  if (!quanteda::is.tokens(x)) {
     stop("x must be a quanteda tokens object")
   }
+  socket <- match.arg(socket)
   
-  cli_h2("Looking up tokens")
-  args = list(...)
-  # exclude the dictionary as argument as it prints out all the tokens in it
-  # Just report anything else by redefining args
-  args = args[!sapply(args, is.dictionary)]
-  if ( length(args) < 1 ) {
-    cli_alert_info("quanteda::tokens_lookup() has been called with the default parameters")
+  args <- list(...)
+  args <- args[!sapply(args, quanteda::is.dictionary)]  # filter dictionaries from logs
+  if (length(args) < 1) {
+    cli::cli_alert_info("quanteda::tokens_lookup() has been called with default parameters")
   } else {
-    cli_alert_info("quanteda::tokens_lookup() has been called with the following parameters")
-    for (nm in names(args)) {
-      cli_alert("{nm} = {toString(args[[nm]])}")
-    }
+    cli::cli_alert_info("quanteda::tokens_lookup() has been called with user parameters")
+    for (nm in names(args)) cli::cli_alert_info("{nm} = {toString(args[[nm]])}")
   }
   
-  # define the number of workers
-  plan(multisession, workers = ncores)
-  chunks = split(x, rep_len(1L:ncores, ndoc(x)))
+  # Split into chunks by doc IDs
+  doc_ids <- quanteda::docnames(x)
+  groups  <- split(doc_ids, rep_len(seq_len(max(1L, nchunks)), length(doc_ids)))
+  chunks  <- lapply(groups, function(ids) if (length(ids)) x[ids] else NULL)
+  chunks  <- Filter(Negate(is.null), chunks)
   
-  toks = do.call(c, future_lapply(chunks, tokens_lookup, future.seed = TRUE, ...))
-  plan(sequential)
-  # ensure original ordering
-  doc_order <- docnames(x)
-  toks <- toks[doc_order]
+  if (length(chunks) <= 1L || ncores < 2L) {
+    cli::cli_alert_info("Lookup sequentially")
+    toks <- quanteda::tokens_lookup(x, ...)
+    return(toks)
+  }
   
-  cli_alert_success("Lookup complete")  
+  cli::cli_alert_info("Lookup {nchunks} chunks in parallel with {ncores} cores and {length(chunks)} chunks")
+  
+  if (socket == "FORK") {
+    if (.Platform$OS.type == "windows") {
+      stop("socket = 'FORK' is not supported on Windows. Use 'PSOCK'.")
+    }
+    warning("FORK sockets may be unstable with quanteda. Consider using 'PSOCK'.")
+    toks_list <- parallel::mclapply(chunks, .lookup_chunk, mc.cores = ncores, ..., SIMPLIFY = FALSE)
+  } else {
+    cl <- parallel::makeCluster(ncores)
+    on.exit(parallel::stopCluster(cl), add = TRUE)
+    parallel::clusterExport(cl, varlist = c(".lookup_chunk"), envir = environment())
+    toks_list <- parallel::clusterApplyLB(cl, chunks, .lookup_chunk, ...)
+  }
+  
+  toks <- Reduce(c, toks_list)
+  toks <- toks[doc_ids]  # restore order
+  
+  cli::cli_alert_success("Lookup complete")
   return(toks)
+}
+
+#' @keywords internal
+.lookup_chunk <- function(tok_chunk, ...) {
+  quanteda::tokens_lookup(tok_chunk, ...)
 }
